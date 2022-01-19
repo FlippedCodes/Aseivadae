0c917aba0dade66621139e1bde154c55749679e2
Phil <philip.schaefer0@gmail.com>
2022-01-20 00:40:40 +0100

    [delete] cleanup unused features

--- functions/FUNC_checkinPostReaction.js
+++ functions/FUNC_checkinPostReaction.js
@@ -1,7 +1,5 @@
 const moment = require('moment');
 
-const userDoB = require('../database/models/UserDoB');
-
 const errHander = (err) => { console.error('ERROR:', err); };
 
 async function getDate(channel, config) {
@@ -16,17 +14,6 @@ async function getDate(channel, config) {
   return moment(rawDate, config.DoBchecking.dateFormats, true);
 }
 
-async function searchUser(ID) {
-  const result = await userDoB.findOne({ where: { ID } }).catch(errHander);
-  return result;
-}
-
-async function addUser(ID, DoB, allow, teammemberID) {
-  if (await userDoB.findOne({ where: { ID } }).catch(errHander)) return false;
-  await userDoB.findOrCreate({ where: { ID }, defaults: { DoB, allow, teammemberID } }).catch(errHander);
-  return true;
-}
-
 module.exports.run = async (client, message, config) => {
   // check if team fore was pinged and if channel is a checkin channel
   if (message.mentions.has(config.teamRole)
@@ -34,17 +21,14 @@ module.exports.run = async (client, message, config) => {
     await message.react('👌');
     await message.react('✋');
     client.functions.get('FUNC_MessageEmbedMessage')
-      .run(null, message.channel,
+      .run(
+        null,
+        message.channel,
         'Please wait for a teammember to review your answers.',
-        null, 4296754, false);
-    if (await searchUser(message.author.id)) await message.react('🔍');
-    else {
-      const date = await getDate(message.channel, config);
-      if (!date || !date.isValid()) return message.react('❓');
-      // add entry
-      await addUser(message.author.id, date.format('YYYY-MM-DD'), false, client.user.id);
-      await message.react('✅');
-    }
+        null,
+        4296754,
+        false,
+      );
   }
 };
 
