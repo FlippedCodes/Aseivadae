527439724f080ef09615ec49d79cb3c232b30505
Phil <philip.schaefer0@gmail.com>
2022-01-21 20:32:13 +0100

    [removed] old code causing crash

--- functions/EVENT_messageReactionAdd.js
+++ functions/EVENT_messageReactionAdd.js
@@ -1,8 +1,6 @@
 module.exports.run = async (reaction, user) => {
   if (user.bot) return;
 
-  // rolerequest
-  client.functions.get('FUNC_userRoleRequest').run(reaction, user);
   // checkin
   client.functions.get('FUNC_checkinCompletedReaction').run(reaction, user);
 };
