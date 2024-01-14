41b120aa430e1dd43a9fb17284337b8651356912
Phil <philip.schaefer0@gmail.com>
2024-01-14 21:40:17 +0100

    add feature link replace

--- functions/EVENT/messageCreate.js
+++ functions/EVENT/messageCreate.js
@@ -3,6 +3,8 @@ module.exports.run = async (message) => {
   if (message.author.bot) return;
   if (message.channel.type === 'dm') return;
 
+  if (config.linkReplace.checkChannels.includes(message.channel.id)) return client.functions.get('ENGINE_linkReplace_check').run(message);
+
   // non command function: checkin complete questioning Reaction adding
   if (message.mentions.roles.has(config.teamRole)
   && message.channel.parentId === config.checkin.categoryID) return client.functions.get('ENGINE_checkin_postReaction').run(message);
